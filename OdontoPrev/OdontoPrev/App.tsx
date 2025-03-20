ae2cffdc2f4010cc3e5ca547183ce2ca04b33be3
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-19 21:51:04 -0300

    feito

--- OdontoPrev/OdontoPrev/App.tsx
+++ OdontoPrev/OdontoPrev/App.tsx
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { Image } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 
 
 
 export default function App() {
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <StatusBar style="auto" />
@@ -19,13 +23,32 @@ export default function App() {
       />
       <Button 
       title="Ir Proxima Tela"
-      onPress={() => alert('Botão pressionado!')}
+      onPress={() => alert('Botão pressionadodsadas!')}
         />
 
+    <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry={true}
+      />
+      
+      
     </View>
     
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -34,4 +57,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: '80%',
+    height: 40,
+    borderBottomColor: '#000',    // Cor do traço
+    borderBottomWidth: 1,         // Espessura do traço
+    paddingHorizontal: 10        // Espaçamento interno
+  }
 });
